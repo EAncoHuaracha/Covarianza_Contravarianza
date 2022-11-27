0d61bbe74ce48b37c000ed70b05b6eed71a1532e
EAncoHuaracha <eancohu@unsa.edu.pe>
2022-11-27 17:52:54 -0500

    creacion de clases X Y

---
+++ c++/X.h
@@ -0,0 +1,8 @@
+#ifndef X_H
+#define X_H
+
+class X {
+  public:
+    virtual B foo(B var)=0;
+};
+#endif
---
+++ c++/Y.h
@@ -0,0 +1,19 @@
+#ifndef Y_H
+#define Y_H
+#include <iostream>
+#include "A.h"
+#include "C.h"
+#include "Clase_B.h"
+#include "Clase_C.h"
+#include "X.h"
+
+class y: public x{
+  public:
+    B* foo(B b){
+      b.beta();
+      B* b2 = new Clase_C();
+      return b2;
+    }
+}
+
+
