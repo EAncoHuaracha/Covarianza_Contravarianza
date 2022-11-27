dfbcdf07924885b1b9fc43707f889268896786c9
EAncoHuaracha <eancohu@unsa.edu.pe>
2022-11-27 16:34:35 -0500

    No acepta el intento de contravarianza

--- Java/Y.java
+++ Java/Y.java
@@ -1,5 +1,6 @@
 public class Y extends X {
 
+    /* 
     @Override
     public B foo(B b) {
         b.beta();
@@ -7,5 +8,15 @@ public class Y extends X {
         B b2 = new Clase_C();
         return b2;
     }
+    */
+
+    public A foo(B b) {
+        b.beta();
+        System.out.println("Hola soy beta desde Y");
+        B b2 = new Clase_C();
+        return b2;
+    }
+
+    
 
 }
