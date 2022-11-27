7b6775d9feee54ef97e3605c4bde538c7afd503e
EAncoHuaracha <eancohu@unsa.edu.pe>
2022-11-27 16:44:41 -0500

    No me deja realizar la covarianza, error me obliga a sobreescribir el metodo de X

--- Java/Y.java
+++ Java/Y.java
@@ -18,12 +18,19 @@ public class Y extends X {
         return b2;
     }
     */
+    /*
     public C foo(B b) {
         b.beta();
         System.out.println("Hola soy beta desde Y");
         C c = new Clase_C();
         return c;
     }
-    
+    */
+    public B foo(C c) {
+        c.beta();
+        System.out.println("Hola soy beta desde Y");
+        B b = new Clase_C();
+        return b;
+    }
 
 }
